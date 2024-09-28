ff171fe629a1883049c51ebc88ae0d4db59fba2f
Sahid Rahutomo <sahid.rahutomo@digitalsekuriti.id>
2024-09-29 01:54:30 +0700

    encahnce  remove file if there was subfolder

--- backup_tele.py
+++ backup_tele.py
@@ -11,16 +11,27 @@ load_dotenv()
 TOKEN = os.getenv('TELEGRAM_TOKEN')  # Your Telegram bot token
 CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')  # Your channel ID
 FOLDER_TO_BACKUP = os.getenv('PATH_FOLDER')  # Path to the folder to back up
+INCLUDE_SUBFOLDER = os.getenv('INCLUDE_SUBFOLDER')  # To determine whether to include subfolders
 
 # Function to send files to Telegram one by one
 async def send_files_to_telegram(folder_path):
     bot = Bot(token=TOKEN)
     try:
-        # List all files in the specified folder
-        files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-        
+        # List to store files to be sent
+        files_to_send = []
+
+        # Iterasi untuk mendapatkan semua file dari folder
+        if INCLUDE_SUBFOLDER == "1":
+            # Jika INCLUDE_SUBFOLDER adalah "1", hanya ambil file di folder utama
+            files_to_send = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+        else:
+            # Jika tidak, ambil file dari folder dan subfolder
+            for root, dirs, files in os.walk(folder_path):
+                for filename in files:
+                    files_to_send.append(os.path.join(root, filename))
+
         # Send each file
-        for file_path in tqdm(files, desc="Sending files", unit="file"):
+        for file_path in tqdm(files_to_send, desc="Sending files", unit="file"):
             with open(file_path, 'rb') as f:
                 await bot.send_document(chat_id=CHANNEL_ID, document=f)
             print(f"File {file_path} successfully sent to the Telegram channel.")
--- remove_file.py
+++ remove_file.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 # Mengonversi COUNT_DAY_REMOVE menjadi integer
 COUNT_DAY_REMOVE = int(os.getenv('COUNT_DAY_REMOVE'))
+INCLUDE_SUBFOLDER = os.getenv('INCLUDE_SUBFOLDER')
 
 def delete_old_files(folder_path):
     # Mendapatkan waktu saat ini
@@ -15,20 +16,25 @@ def delete_old_files(folder_path):
     days_in_seconds = COUNT_DAY_REMOVE * 24 * 60 * 60
     deleted_files_count = 0  # Untuk menghitung jumlah file yang dihapus
 
-    # Iterasi melalui semua file dalam folder
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
+    # Iterasi melalui semua item dalam folder
+    for root, dirs, files in os.walk(folder_path):
+        if INCLUDE_SUBFOLDER == "1" and root != folder_path:
+            # Jika INCLUDE_SUBFOLDER adalah "1", jangan proses subfolder
+            continue
 
-        # Memastikan hanya memproses file
-        if os.path.isfile(file_path):
-            # Mengambil waktu modifikasi file
-            file_age = now - os.path.getmtime(file_path)
-            
-            # Jika file lebih tua dari jumlah hari yang ditentukan, hapus file tersebut
-            if file_age > days_in_seconds:
-                os.remove(file_path)
-                print(f'Deleted: {file_path}')
-                deleted_files_count += 1
+        for filename in files:
+            file_path = os.path.join(root, filename)
+
+            # Memastikan hanya memproses file
+            if os.path.isfile(file_path):
+                # Mengambil waktu modifikasi file
+                file_age = now - os.path.getmtime(file_path)
+                
+                # Jika file lebih tua dari jumlah hari yang ditentukan, hapus file tersebut
+                if file_age > days_in_seconds:
+                    os.remove(file_path)
+                    print(f'Deleted: {file_path}')
+                    deleted_files_count += 1
 
     # Memberikan informasi jika tidak ada file yang dihapus
     if deleted_files_count == 0:
